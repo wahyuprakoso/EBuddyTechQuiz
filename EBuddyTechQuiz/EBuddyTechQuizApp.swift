2453687153396f76592a28d214fe039393f59598
Wahyu Prakoso <wahyu@codelabs.co.id>
2024-12-30 07:30:56 +0700

    Initial Commit

---
+++ EBuddyTechQuiz/EBuddyTechQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  EBuddyTechQuizApp.swift
+//  EBuddyTechQuiz
+//
+//  Created by Wahyu Prakoso on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EBuddyTechQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EBuddyTechQuizTests/EBuddyTechQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  EBuddyTechQuizTests.swift
+//  EBuddyTechQuizTests
+//
+//  Created by Wahyu Prakoso on 30/12/24.
+//
+
+import Testing
+@testable import EBuddyTechQuiz
+
+struct EBuddyTechQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
