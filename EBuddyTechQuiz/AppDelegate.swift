a699e35720b18edb81d01fce479fd49d95e3ab0f
Wahyu Prakoso <wahyu@codelabs.co.id>
2024-12-30 09:35:16 +0700

    feat: setup firebase, firestore, auth, storage

---
+++ EBuddyTechQuiz/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  EBuddyTechQuiz
+//
+//  Created by Wahyu Prakoso on 30/12/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
--- EBuddyTechQuiz/EBuddyTechQuizApp.swift
+++ EBuddyTechQuiz/EBuddyTechQuizApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct EBuddyTechQuizApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
